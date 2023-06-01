309a1c3e3d1175d16beb3d3baca12d9419612076
pdmsk <102367559+pdmsk@users.noreply.github.com>
2023-06-01 16:09:04 +0200

    changed strings

--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSimpleFactory(t *testing.T) {
     f := SimpleFactory("http://localhost:4444")
 
-    if f.Url != "http://localhost&quot; {
-        t.Errorf("feature incorrect, got %s, want: %s ", f.Url, "http://localhost:4444)
+    if f.Url != "http://localhost:4444" {
+        t.Errorf("feature incorrect, got %s, want: %s ", f.Url, "http://localhost:4444")
     }
 }
