2859bc69835224482e70567f1f853bd8bc717c39
pdmsk <102367559+pdmsk@users.noreply.github.com>
2023-06-01 15:56:27 +0200

    changed tests back

--- main_test.go
+++ main_test.go
@@ -1,34 +1,11 @@
 package main
 
-import (
-    "net/http"
-    "net/http/httptest"
-    "testing"
-)
+import "testing"
 
-func TestHandler(t *testing.T) {
-    req, err := http.NewRequest("GET", "/api", nil)
-    if err != nil {
-        t.Fatal(err)
+func TestSimpleFactory(t *testing.T) {
+    f := SimpleFactory("http://localhost:4444)
+
+    if f.Url != "http://localhost&quot; {
+        t.Errorf("feature incorrect, got %s, want: %s ", f.Url, "http://localhost:4444)
     }
-
-    rr := httptest.NewRecorder()
-    handler := http.HandlerFunc(handler)
-
-    handler.ServeHTTP(rr, req)
-
-    if status := rr.Code; status != http.StatusOK {
-        t.Errorf("handler returned wrong status code: got %v want %v",
-            status, http.StatusOK)
-    }
-
-    expected := `{"Name":"Artsiom","Description":"World","Url":"localhost:4444"}`
-    if rr.Body.String() != expected {
-        t.Errorf("handler returned unexpected body: got %v want %v",
-            rr.Body.String(), expected)
-    }
-}
-
-func TestMainFunction(t *testing.T) {
-    go main()
 }
