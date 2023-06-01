63dfd92057269fd954a27b541a1b4e3614339abd
pdmsk <102367559+pdmsk@users.noreply.github.com>
2023-06-01 15:41:03 +0200

    changed tests

--- main_test.go
+++ main_test.go
@@ -1,11 +1,35 @@
 package main
 
-import "testing"
+import (
+    "encoding/json"
+    "net/http"
+    "net/http/httptest"
+    "testing"
+)
 
-func TestSimpleFactory(t *testing.T) {
-    f := SimpleFactory("http://localhost&quot;)
-
-    if f.Url != "http://localhost&quot; {
-        t.Errorf("feature incorrect, got %s, want: %s ", f.Url, "http://localhost&quot;)
+func TestHandler(t *testing.T) {
+    req, err := http.NewRequest("GET", "/api", nil)
+    if err != nil {
+        t.Fatal(err)
     }
+
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(handler)
+
+    handler.ServeHTTP(rr, req)
+
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf("handler returned wrong status code: got %v want %v",
+            status, http.StatusOK)
+    }
+
+    expected := `{"Name":"Your Name","Description":"World","Url":"localhost:4444"}`
+    if rr.Body.String() != expected {
+        t.Errorf("handler returned unexpected body: got %v want %v",
+            rr.Body.String(), expected)
+    }
+}
+
+func TestMainFunction(t *testing.T) {
+    go main()
 }
