63fefb743e338034829ecafacc0eec0b3834c3e1
pdmsk <102367559+pdmsk@users.noreply.github.com>
2023-06-01 16:13:43 +0200

    changed name

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSimpleFactory(t *testing.T) {
-    f := SimpleFactory("http://localhost:4444")
+    f := Simple("http://localhost:4444")
 
     if f.Url != "http://localhost:4444" {
         t.Errorf("feature incorrect, got %s, want: %s ", f.Url, "http://localhost:4444")
