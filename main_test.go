a32c39e65e69e362ccdafd35a67cf54340df1623
pdmsk <102367559+pdmsk@users.noreply.github.com>
2023-06-01 16:07:37 +0200

    added bracket

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSimpleFactory(t *testing.T) {
-    f := SimpleFactory("http://localhost:4444)
+    f := SimpleFactory("http://localhost:4444")
 
     if f.Url != "http://localhost&quot; {
         t.Errorf("feature incorrect, got %s, want: %s ", f.Url, "http://localhost:4444)
