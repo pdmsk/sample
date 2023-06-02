88e411d3c2955c66cbdca180176ce1f4eed04552
pdmsk <102367559+pdmsk@users.noreply.github.com>
2023-06-02 13:27:57 +0200

    added failed testcase

--- main_test.go
+++ main_test.go
@@ -7,5 +7,7 @@ func TestSimpleFactory(t *testing.T) {
 
     if f.Url != "http://localhost:4444" {
         t.Errorf("feature incorrect, got %s, want: %s ", f.Url, "http://localhost:4444")
+    } else {
+        t.Logf("TestSimpleFactory: Passed")
     }
 }
